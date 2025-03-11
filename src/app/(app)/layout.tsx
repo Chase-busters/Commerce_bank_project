89e3a34b7cea2bf4eaab29f5ac8b2f28f9dbbdfe
Isaac <isaacspiegel@gmail.com>
2025-03-11 13:49:58 -0500

    Add page layout Fixes #15

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,19 @@
 import type { Metadata } from 'next'
-import { Fragment } from 'react';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+import { UserButton } from '@clerk/nextjs';
+import { NavLink } from './NavLink';
 
 export const metadata: Metadata = {
   title: 'ChaseBuster app',
   description: 'Descriptive description',
 }
+
+const links = [
+  { name: 'Home', href: '/' },
+  { name: 'Accounts', href: '/accounts' },
+  { name: 'Transactions', href: '/transactions' },
+];
 
 export default async function AppLayout({
   children,
@@ -14,17 +21,44 @@ export default async function AppLayout({
   children: React.ReactNode
 }>) {
   const { userId } = await auth()
-
   const isSignedIn = !!userId
-
-
   if (!isSignedIn) {
     redirect('/sign-in')
   }
+  return (
+    <div className="flex flex-col h-screen relative">
+      <Navbar
+        links={links}
+      />
+      {/* Offset for the Navbar */}
+      <div className="pt-16 h-full w-full">
+        <main className="px-2 lg:px-6 h-full w-full relative">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export function Navbar({
+  links,
+}: {
+  links: {
+    name: string,
+    href: string,
+  }[],
+}) {
 
   return (
-    <Fragment>
-      {children}
-    </Fragment>
-  )
+    <div className="absolute z-50 w-full h-full left-0 top-0 flex flex-col pointer-events-none">
+      <nav className="h-16 w-full bg-gray-800 border-b border border-gray-900 text-white flex items-center justify-between px-4 pointer-events-auto drop-shadow-xl">
+        <ul className="flex space-x-4">
+          {links.map(link => (
+            <NavLink {...link} key={link.href} />
+          ))}
+        </ul>
+        <UserButton />
+      </nav>
+    </div>
+  );
 }
